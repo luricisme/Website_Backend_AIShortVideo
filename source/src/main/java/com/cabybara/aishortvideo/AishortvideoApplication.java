436768b8baf60d5a1a0930cd1fca64edab11d9ab
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-07 20:09:59 +0700

    Initial folder

---
+++ source/src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
@@ -0,0 +1,13 @@
+package com.cabybara.aishortvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AishortvideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AishortvideoApplication.class, args);
+	}
+
+}
---
+++ source/src/test/java/com/cabybara/aishortvideo/AishortvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabybara.aishortvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AishortvideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
