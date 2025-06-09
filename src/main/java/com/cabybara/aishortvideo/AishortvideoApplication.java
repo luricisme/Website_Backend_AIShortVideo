4454bd3bb0aadb7cecfa396550516fefee18310d
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-09 23:29:03 +0700

    Change folder's structure

--- src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
+++ src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
@@ -2,9 +2,11 @@ package com.cabybara.aishortvideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AishortvideoApplication {
+	// Test CI
 
 	public static void main(String[] args) {
 		SpringApplication.run(AishortvideoApplication.class, args);
