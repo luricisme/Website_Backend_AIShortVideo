16e080883d3568668c88530be775395826d5db84
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-16 16:21:34 +0700

    Add Swagger api config

--- src/main/java/com/cabybara/aishortvideo/controller/TestController.java
+++ src/main/java/com/cabybara/aishortvideo/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.cabybara.aishortvideo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Tag(name = "Test Controller")
 public class TestController {
+    @Operation(summary= "Summary hello world", description = "Get hello world")
     @GetMapping(value = "/hello-world")
     public String helloWorld() {
         return "Hello World!";
