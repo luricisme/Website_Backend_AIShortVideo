a23bbce80d6cf6164bcd42cb9337fc1147faaa81
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-24 13:29:52 +0700

    Upload file from client and return URL

---
+++ src/main/java/com/cabybara/aishortvideo/service/create_video/implement/SaveFileServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cabybara.aishortvideo.service.create_video.implement;
+
+import com.cabybara.aishortvideo.dto.response.create_video.SaveFileResponseDTO;
+import com.cabybara.aishortvideo.service.cloud.CloudinaryService;
+import com.cabybara.aishortvideo.service.create_video.SaveFileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SaveFileServiceImpl implements SaveFileService {
+    private final CloudinaryService cloudinaryService;
+
+    @Override
+    public SaveFileResponseDTO saveFile(MultipartFile file, String type) throws IOException {
+        String fileURL = "";
+        String filename = "";
+        if ("audio".equals(type)) {
+            filename = "user_audio" + UUID.randomUUID() + ".mp3";
+            fileURL = cloudinaryService.uploadMultipartFile(file, filename,"raw");
+        } else if ("video".equals(type)) {
+            filename = "ai_video" + UUID.randomUUID();
+            fileURL = cloudinaryService.uploadMultipartFile(file, filename,"video");
+        }
+        return SaveFileResponseDTO.builder()
+                .fileURL(fileURL)
+                .build();
+    }
+}
