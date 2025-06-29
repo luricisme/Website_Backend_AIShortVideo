066e6b8a9e68cc8965ff074e85de7f28a473ecaa
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-29 19:23:56 +0700

    Setup key in env and github

--- src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
+++ src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
@@ -1,15 +1,14 @@
 package com.cabybara.aishortvideo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AishortvideoApplication {
-	// Test CI
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AishortvideoApplication.class, args);
 	}
-
 }
