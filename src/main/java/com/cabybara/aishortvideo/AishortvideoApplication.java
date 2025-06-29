7f6e09b66f664d75dfb25661e9c7a443b500cc55
luricisme <trinhnguyenluong182306@gmail.com>
2025-06-29 19:31:49 +0700

    Fix CI env

--- src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
+++ src/main/java/com/cabybara/aishortvideo/AishortvideoApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AishortvideoApplication {
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		SpringApplication.run(AishortvideoApplication.class, args);
-	}
+    public static void main(String[] args) {
+//		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+//		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        if (System.getenv("CI") == null) { // GitHub Actions có env CI=true
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        }
+        SpringApplication.run(AishortvideoApplication.class, args);
+    }
 }
