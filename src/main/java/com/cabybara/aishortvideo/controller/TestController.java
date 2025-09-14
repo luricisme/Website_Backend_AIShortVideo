39a1b37641fe2d55044453641ce1f1cf45e73405
luricisme <trinhnguyenluong182306@gmail.com>
2025-09-15 01:03:31 +0700

    Modify database

--- src/main/java/com/cabybara/aishortvideo/controller/TestController.java
+++ src/main/java/com/cabybara/aishortvideo/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Test 
 @RestController
 @RequestMapping("/")
 @Tag(name = "Test Controller")
